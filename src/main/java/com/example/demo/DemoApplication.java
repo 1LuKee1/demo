50dff0ef3a8b9f3becc00417f17cec3e6c00dfd9
Łukasz Kręc <lukaszkrec.id@gmail.com>
2022-12-08 21:07:26 +0100

    Final commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+<<<<<<< HEAD
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+=======
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hello world");
+
+		
+
+    }
+>>>>>>> feturebranch1
 
 }
